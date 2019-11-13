fc0cc6be6f8a7482a2d369761a5eb4ed367bcd92
Edivania Pontes <edivaniapts@gmail.com>
2019-11-13 19:46:09 -0300

    mostrar msg erro ao inserir

--- src/views/pages/Register.vue
+++ src/views/pages/Register.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="app flex-row align-items-center">
     <div class="container">
+
+    <p v-if="errors.length">
+    <b-alert v-for="error in errors" variant="danger" show>
+      {{ error }}
+    </b-alert>
+    </p>
+
       <b-row class="justify-content-center">
         <b-col md="6" sm="8">
           <b-card no-body class="mx-4">
@@ -12,43 +19,43 @@
                   <b-input-group-prepend>
                     <b-input-group-text><i class="icon-user"></i></b-input-group-text>
                   </b-input-group-prepend>
-                  <b-form-input type="text" v-model = "nome" class="form-control" placeholder="Nome Completo" autocomplete="nome" />
+                  <b-form-input type="text" v-model="nome" class="form-control" placeholder="Nome Completo" autocomplete="nome" required/>
                 </b-input-group>
 
                 <b-input-group class="mb-3">
                   <b-input-group-prepend>
                     <b-input-group-text>123</b-input-group-text>
                   </b-input-group-prepend>
-                  <b-form-input type="text" v-model = "cpf" class="form-control" placeholder="CPF" autocomplete="cpf" />
+                  <b-form-input type="text" v-model = "cpf" class="form-control" placeholder="CPF" autocomplete="cpf" required/>
                 </b-input-group>
 
                 <b-input-group class="mb-3">
                   <b-input-group-prepend>
                     <b-input-group-text>@</b-input-group-text>
                   </b-input-group-prepend>
-                  <b-form-input type="text" v-model = "email" class="form-control" placeholder="Email" autocomplete="email" />
+                  <b-form-input type="email" v-model = "email" class="form-control" placeholder="Email" autocomplete="email" required/>
                 </b-input-group>
 
                 <b-input-group class="mb-3">
                   <b-input-group-prepend>
                     <b-input-group-text><i class="icon-lock"></i></b-input-group-text>
                   </b-input-group-prepend>
-                  <b-form-input type="password" v-model = "senha" class="form-control" placeholder="Senha" autocomplete="new-password" />
+                  <b-form-input type="password" v-model = "senha" class="form-control" placeholder="Senha" autocomplete="new-password" required/>
                 </b-input-group>
 
                 <b-input-group class="mb-4">
                   <b-input-group-prepend>
                     <b-input-group-text><i class="icon-lock"></i></b-input-group-text>
                   </b-input-group-prepend>
-                  <b-form-input v-model = "confirmar" type="password" class="form-control" placeholder="Confirme a senha" autocomplete="new-password" />
+                  <b-form-input v-model="confirmar_senha" type="password" class="form-control" placeholder="Confirme a senha" autocomplete="new-password" required/>
                 </b-input-group>
 
-                <b-button variant="success" block @click="cadastrar()" >Finalizar Cadastro</b-button>
+                <b-button variant="success" block @click="cadastrar()"> Finalizar Cadastro </b-button>
 
-                <b-button variant="danger" block to="/pages/login">Cancelar</b-button>
+                <b-button variant="danger" block to="/pages/login"> Cancelar </b-button>
               </b-form>
             </b-card-body>
-            
+
           </b-card>
         </b-col>
       </b-row>
@@ -59,30 +66,39 @@
 <script>
 import axios from 'axios';
 import qs from 'qs';
+
 export default {
   name: 'Register',
   data(){
-    return {email: "", nome: "", cpf: "", senha: "", confirmar: ""}
+    return {
+      email: "",
+      nome: "",
+      cpf: "",
+      senha: "",
+      confirmar_senha: "",
+      errors: []
+      }
   },
   methods:{
     cadastrar(){
-      if (this.email != "" && this.nome != "" && this.cpf != "" 
-      && this.senha != "" && this.senha == this.confirmar){
-        const data = qs.stringify({email: this.email, senha: this.senha, nome: this.nome
-        , cpf: this.cpf})
-        const header = {'content-type': 'application/x-www-form-urlencoded;charset=utf-8'}
-        axios.post(process.env.VUE_APP_API+"/usuario/registrar", data, header).then((response) =>{
-          if (response.status == 201){
-            this.$router.push('/pages/login')
-          }else{
-            //usuário existente ou dados inválidos
+      this.errors = []
+
+          if(this.senha == this.confirmar_senha){
+            const data = qs.stringify({email: this.email, senha: this.senha, nome: this.nome, cpf: this.cpf})
+            const header = {'content-type': 'application/x-www-form-urlencoded;charset=utf-8'}
+            axios.post(process.env.VUE_APP_API+"/usuario/registrar", data, header).then((response) =>{
+              if (response.status == 201){
+                this.$router.push('/pages/login')
+              }else{
+                this.errors.push(response.status)
+              }
+            }).catch(()=>{
+              this.$router.push('/pages/500')
+            })
+          } else {
+          this.errors.push("As senhas nao conferem")
           }
-        }).catch(()=>{
-          this.$router.push('/pages/500')
-        })
-      }
+        }
     }
   }
-
-}
 </script>
