8bfa338e4d89d3531aba8db62f19895d5b1f9e84
Edivania Pontes <edivaniapts@gmail.com>
2019-11-14 01:32:10 -0300

    ajustando mensagens de erros para toast

--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -1,94 +1,125 @@
+
+
 <template>
-  <div class="app flex-row align-items-center">
+
+<div class="app flex-row align-items-center">
     <div class="container">
 
-    <p v-if="errors.length">
-    <b-alert v-for="error in errors" variant="danger" show>
-      {{ error }}
-    </b-alert>
-    </p>
+        <!-- nao esta sendo usado agora, troquei pelo toast -->
+        <p v-if="errors.length">
+            <b-alert v-for="error in errors" variant="danger" show>
+                {{ error }}
+            </b-alert>
+        </p>
 
-      <b-row class="justify-content-center">
-        <b-col md="8">
-          <b-card-group>
-            <b-card no-body class="p-4">
-              <b-card-body>
-                <b-form>
-                  <h1>Login</h1>
-                  <p class="text-muted">Sign In to your account</p>
-                  <b-input-group class="mb-3">
-                    <b-input-group-prepend><b-input-group-text><i class="icon-user"></i></b-input-group-text></b-input-group-prepend>
-                    <b-form-input v-model = "email" type="text" class="form-control" placeholder="Email" autocomplete="username email" />
-                  </b-input-group>
-                  <b-input-group class="mb-4">
-                    <b-input-group-prepend><b-input-group-text><i class="icon-lock"></i></b-input-group-text></b-input-group-prepend>
-                    <b-form-input v-model = "password" type="password" class="form-control" placeholder="Senha" autocomplete="current-password" />
-                  </b-input-group>
-                  <b-row>
-                    <b-col cols="6">
-                      <b-button @click="logar()" variant="primary" class="px-4">Login</b-button>
-                    </b-col>
-                    <b-col cols="6" class="text-right">
-                      <b-button variant="link" class="px-0">Esqueceu sua senha?</b-button>
-                    </b-col>
-                  </b-row>
-                </b-form>
-              </b-card-body>
-            </b-card>
-            <b-card no-body class="text-white bg-primary py-5 d-md-down-none" style="width:44%">
-              <b-card-body class="text-center">
-                <div>
-                  <h2>Ainda não possui sua conta?</h2>
-                  <p>Venha fazer parte do MeFormando e participe da sua formatura.</p>
-                  <b-button variant="primary" class="active mt-3" to="/pages/register">Registre-se agora!</b-button>
-                </div>
-              </b-card-body>
-              <b-button v-if="DeuCerto">
-                True
-              </b-button>
-            </b-card>
-          </b-card-group>
-        </b-col>
-      </b-row>
+        <b-row class="justify-content-center">
+            <b-col md="8">
+                <b-card-group>
+                    <b-card no-body class="p-4">
+                        <b-card-body>
+                            <b-form>
+                                <h1>Login</h1>
+                                <p class="text-muted">Sign In to your account</p>
+                                <b-input-group class="mb-3">
+                                    <b-input-group-prepend>
+                                        <b-input-group-text><i class="icon-user"></i></b-input-group-text>
+                                    </b-input-group-prepend>
+                                    <b-form-input v-model="email" type="text" class="form-control" placeholder="Email" autocomplete="username email" />
+                                </b-input-group>
+                                <b-input-group class="mb-4">
+                                    <b-input-group-prepend>
+                                        <b-input-group-text><i class="icon-lock"></i></b-input-group-text>
+                                    </b-input-group-prepend>
+                                    <b-form-input v-model="password" type="password" class="form-control" placeholder="Senha" autocomplete="current-password" />
+                                </b-input-group>
+                                <b-row>
+                                    <b-col cols="6">
+                                        <b-button @click="logar()" variant="primary" class="px-4">Login</b-button>
+                                    </b-col>
+                                    <b-col cols="6" class="text-right">
+                                        <b-button variant="link" class="px-0">Esqueceu sua senha?</b-button>
+                                    </b-col>
+                                </b-row>
+                            </b-form>
+                        </b-card-body>
+                    </b-card>
+                    <b-card no-body class="text-white bg-primary py-5 d-md-down-none" style="width:44%">
+                        <b-card-body class="text-center">
+                            <div>
+                                <h2>Ainda não possui sua conta?</h2>
+                                <p>Venha fazer parte do MeFormando e participe da sua formatura.</p>
+                                <b-button variant="primary" class="active mt-3" to="/pages/register">Registre-se agora!</b-button>
+                            </div>
+                        </b-card-body>
+                        <b-button v-if="DeuCerto">
+                            True
+                        </b-button>
+                    </b-card>
+                </b-card-group>
+            </b-col>
+        </b-row>
     </div>
-  </div>
+</div>
+
 </template>
 
 <script>
+
 import axios from 'axios';
 import qs from 'qs';
 export default {
-  name: 'Login',
-  data() {
-    return {
-      password: "",
-      email: "",
-      errors: []
-      }
-  },
-  props:{
-    DeuCerto:{
-      type:Boolean,
-      default: false
-    }
-  },
-  methods: {
-    logar(){
-    this.errors = []
-      const data = qs.stringify({email: this.email, senha: this.password})
-      const header = {'content-type': 'application/x-www-form-urlencoded;charset=utf-8'}
-      axios.post(process.env.VUE_APP_API+"/usuario/logar", data, header).then((response) =>{
-        if (response.status == 201){
-          localStorage.setItem("user_token", response.headers.token)
-          this.$router.push('/')
-        }else{
-        this.errors.push(response.headers.erro)
+    name: 'Login',
+    data() {
+        return {
+            password: "",
+            email: "",
+            errors: []
         }
-      }).catch(()=>{
-        this.errors.push("Desculpe, algo deu errado")
-
-      })
+    },
+    props: {
+        DeuCerto: {
+            type: Boolean,
+            default: false
+        }
+    },
+    methods: {
+        logar() {
+            this.errors = []
+            const data = qs.stringify({
+                email: this.email,
+                senha: this.password
+            })
+            const header = {
+                'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+            }
+            axios.post(process.env.VUE_APP_API + "/usuario/logar", data, header).then((response) => {
+                if (response.status == 201) {
+                    localStorage.setItem("user_token", response.headers.token)
+                    this.$router.push('/')
+                } else {
+                    //this.errors.push(response.headers.erro)
+                    this.$bvToast.toast(response.headers.erro, {
+                        title: "Erro:",
+                        variant: "danger",
+                        toaster: "b-toaster-top-center",
+                        solid: true,
+                        autoHideDelay: 10000,
+                        appendToast: true
+                    })
+                }
+            }).catch(() => {
+                //this.errors.push("Desculpe, algo deu errado")
+                this.$bvToast.toast("Desculpe, algo deu errado", {
+                    title: "Erro:",
+                    variant: "warning",
+                    toaster: "b-toaster-top-center",
+                    solid: true,
+                    autoHideDelay: 8000,
+                    appendToast: true
+                })
+            })
+        }
     }
-  }
 }
+
 </script>
--- src/views/pages/Register.vue
+++ src/views/pages/Register.vue
@@ -1,104 +1,137 @@
 <template>
-  <div class="app flex-row align-items-center">
+
+<div class="app flex-row align-items-center">
     <div class="container">
 
-    <p v-if="errors.length">
-    <b-alert v-for="error in errors" variant="danger" show>
-      {{ error }}
-    </b-alert>
-    </p>
+        <!-- nao esta sendo usado agora, troquei pelo toast -->
+        <p v-if="errors.length">
+            <b-alert v-for="error in errors" variant="danger" show>
+                {{ error }}
+            </b-alert>
+        </p>
 
-      <b-row class="justify-content-center">
-        <b-col md="6" sm="8">
-          <b-card no-body class="mx-4">
-            <b-card-body class="p-4">
-              <b-form>
-                <h1>Registro</h1>
-                <p class="text-muted">Crie sua conta</p>
-                <b-input-group class="mb-3">
-                  <b-input-group-prepend>
-                    <b-input-group-text><i class="icon-user"></i></b-input-group-text>
-                  </b-input-group-prepend>
-                  <b-form-input type="text" v-model="nome" class="form-control" placeholder="Nome Completo" autocomplete="nome" required/>
-                </b-input-group>
+        <b-row class="justify-content-center">
+            <b-col md="6" sm="8">
+                <b-card no-body class="mx-4">
+                    <b-card-body class="p-4">
+                        <b-form>
+                            <h1>Registro</h1>
+                            <p class="text-muted">Crie sua conta</p>
+                            <b-input-group class="mb-3">
+                                <b-input-group-prepend>
+                                    <b-input-group-text><i class="icon-user"></i></b-input-group-text>
+                                </b-input-group-prepend>
+                                <b-form-input type="text" v-model="nome" class="form-control" placeholder="Nome Completo" autocomplete="nome" required/>
+                            </b-input-group>
 
-                <b-input-group class="mb-3">
-                  <b-input-group-prepend>
-                    <b-input-group-text>123</b-input-group-text>
-                  </b-input-group-prepend>
-                  <b-form-input type="text" v-model = "cpf" class="form-control" placeholder="CPF" autocomplete="cpf" required/>
-                </b-input-group>
+                            <b-input-group class="mb-3">
+                                <b-input-group-prepend>
+                                    <b-input-group-text>123</b-input-group-text>
+                                </b-input-group-prepend>
+                                <b-form-input type="text" v-model="cpf" class="form-control" placeholder="CPF" autocomplete="cpf" required/>
+                            </b-input-group>
 
-                <b-input-group class="mb-3">
-                  <b-input-group-prepend>
-                    <b-input-group-text>@</b-input-group-text>
-                  </b-input-group-prepend>
-                  <b-form-input type="email" v-model = "email" class="form-control" placeholder="Email" autocomplete="email" required/>
-                </b-input-group>
+                            <b-input-group class="mb-3">
+                                <b-input-group-prepend>
+                                    <b-input-group-text>@</b-input-group-text>
+                                </b-input-group-prepend>
+                                <b-form-input type="email" v-model="email" class="form-control" placeholder="Email" autocomplete="email" required/>
+                            </b-input-group>
 
-                <b-input-group class="mb-3">
-                  <b-input-group-prepend>
-                    <b-input-group-text><i class="icon-lock"></i></b-input-group-text>
-                  </b-input-group-prepend>
-                  <b-form-input type="password" v-model = "senha" class="form-control" placeholder="Senha" autocomplete="new-password" required/>
-                </b-input-group>
+                            <b-input-group class="mb-3">
+                                <b-input-group-prepend>
+                                    <b-input-group-text><i class="icon-lock"></i></b-input-group-text>
+                                </b-input-group-prepend>
+                                <b-form-input type="password" v-model="senha" class="form-control" placeholder="Senha" autocomplete="new-password" required/>
+                            </b-input-group>
+                            <p class="text-muted">Mínimo de 8 caracteres</p>
 
-                <b-input-group class="mb-4">
-                  <b-input-group-prepend>
-                    <b-input-group-text><i class="icon-lock"></i></b-input-group-text>
-                  </b-input-group-prepend>
-                  <b-form-input v-model="confirmar_senha" type="password" class="form-control" placeholder="Confirme a senha" autocomplete="new-password" required/>
-                </b-input-group>
+                            <b-input-group class="mb-4">
+                                <b-input-group-prepend>
+                                    <b-input-group-text><i class="icon-lock"></i></b-input-group-text>
+                                </b-input-group-prepend>
+                                <b-form-input v-model="confirmar_senha" type="password" class="form-control" placeholder="Confirme a senha" autocomplete="new-password" required/>
+                            </b-input-group>
 
-                <b-button variant="success" block @click="cadastrar()"> Finalizar Cadastro </b-button>
+                            <b-button variant="success" block @click="cadastrar()"> Finalizar Cadastro </b-button>
 
-                <b-button variant="danger" block to="/pages/login"> Cancelar </b-button>
-              </b-form>
-            </b-card-body>
+                            <b-button variant="danger" block to="/pages/login"> Cancelar </b-button>
+                        </b-form>
+                    </b-card-body>
 
-          </b-card>
-        </b-col>
-      </b-row>
+                </b-card>
+            </b-col>
+        </b-row>
     </div>
-  </div>
+</div>
+
 </template>
 
 <script>
+
 import axios from 'axios';
 import qs from 'qs';
 
 export default {
-  name: 'Register',
-  data(){
-    return {
-      email: "",
-      nome: "",
-      cpf: "",
-      senha: "",
-      confirmar_senha: "",
-      errors: []
-      }
-  },
-  methods:{
-    cadastrar(){
-      this.errors = []
+    name: 'Register',
+    data() {
+        return {
+            email: "",
+            nome: "",
+            cpf: "",
+            senha: "",
+            confirmar_senha: "",
+            errors: []
+        }
+    },
+    methods: {
+        cadastrar() {
+            this.errors = []
 
-          if(this.senha == this.confirmar_senha){
-            const data = qs.stringify({email: this.email, senha: this.senha, nome: this.nome, cpf: this.cpf})
-            const header = {'content-type': 'application/x-www-form-urlencoded;charset=utf-8'}
-            axios.post(process.env.VUE_APP_API+"/usuario/registrar", data, header).then((response) =>{
-              if (response.status == 201){
-                this.$router.push('/pages/login')
-              }else{
-                this.errors.push(response.headers.erro)
-              }
-            }).catch(()=>{
-              this.$router.push('/pages/500')
-            })
-          } else {
-          this.errors.push("As senhas nao conferem")
-          }
+            if (this.senha == this.confirmar_senha) {
+                const data = qs.stringify({
+                    email: this.email,
+                    senha: this.senha,
+                    nome: this.nome,
+                    cpf: this.cpf
+                })
+                const header = {
+                    'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+                }
+                axios.post(process.env.VUE_APP_API + "/usuario/registrar", data, header).then((response) => {
+                    if (response.status == 201) {
+                        this.$router.push('/pages/login')
+                    } else {
+                        //nao usado agora, trocado pelo toast
+                        //this.errors.push(response.headers.erro)
+
+                        this.$bvToast.toast(response.headers.erro, {
+                            title: "Erro:",
+                            variant: "danger",
+                            toaster: "b-toaster-top-center",
+                            solid: true,
+                            autoHideDelay: 8000,
+                            appendToast: true
+                        })
+                    }
+                }).catch(() => {
+                    this.$router.push('/pages/500')
+                })
+            } else {
+                //nao usado agora, trocado pelo toast
+                //this.errors.push("As senhas nao conferem")
+
+                this.$bvToast.toast("As senhas nao conferem", {
+                    title: "Aviso:",
+                    variant: "warning",
+                    toaster: "b-toaster-top-center",
+                    solid: true,
+                    autoHideDelay: 8000,
+                    appendToast: true
+                })
+            }
         }
     }
-  }
+}
+
 </script>
