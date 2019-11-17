810aec9f2dc8e9591b6df4eed61432cc5e855490
Joicy Oliveira <joiicyoliv@gmail.com>
2019-11-17 01:39:44 -0300

    validando evento

--- src/views/AdicionarEvento.vue
+++ src/views/AdicionarEvento.vue
@@ -66,7 +66,17 @@ export default {
   data () {
     return {token:"",Titulo: "", Custo: "",Date: "", Descricao: ""}
   },
-  methods: {
+   methods: {
+    makeToast(title = null, text = null, variant = null) {
+      this.$bvToast.toast(text, {
+        title: title,
+        variant: variant,
+        toaster: "b-toaster-top-center",
+        solid: true,
+        autoHideDelay: 8000,
+        appendToast: true
+      });
+    },
     criarEvento(){
     if (this.Titulo != "" && this.Custo != "" && this.dataAtual  != null ){
         const data = qs.stringify({token: localStorage.getItem('user_token') ,Titulo: this.Titulo, Custo: this.Custo , Date: this.dataAtual, Descricao: this.Descricao})
@@ -76,14 +86,13 @@ export default {
             if (response.status == 201){
                 this.$router.push('/eventos') 
             }else{
-                
-                alert("Erro")
+                this.makeToast("Erro", response.headers.erro, "danger")
             }
         }).catch(()=>{
-            alert("Erro catch")
+             this.makeToast("Aviso", "Erro ao conectar com a API", "warning")
         })
     }else{
-        alert("Não tem todos os campos!!!")
+        this.makeToast("Erro", "Preencha todos os campos", "danger")
     }
     }
   }
